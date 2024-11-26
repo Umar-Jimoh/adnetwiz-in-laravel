ba6336dc944e6d4eeabe4fa16d01a48ccd0a10dc
Umar Jimoh <umarjimoh@hotmail.com>
2024-11-26 12:17:45 +0100

    Add category show view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
 class CategoryController extends Controller
 {
-    function show (string $slug) {
-        return $slug;
+    function show(string $slug): View
+    {
+        return view('category.show');
     }
 }
