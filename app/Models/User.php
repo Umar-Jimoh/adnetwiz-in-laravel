892f704980d443add24c928b728cc8ac2a5dec8c
Umar Jimoh <umarjimoh@hotmail.com>
2024-12-05 16:09:42 +0100

    Set role default to reader

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $attributes = [
+        'role'=>'reader',
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
