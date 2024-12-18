a2ccc4b97eb5d376c7c2d63d0c8d8af8b74c3e91
Umar Jimoh <umarjimoh@hotmail.com>
2024-12-18 12:09:38 +0100

    Add mass assignment protection to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+
+    protected $fillable = ['title', 'category_id', 'thumbnail', 'content'];
+    
     public function user():BelongsTo {
 
         return $this->belongsTo(User::class);
