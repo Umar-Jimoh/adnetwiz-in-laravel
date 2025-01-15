1024cd413bd8feecf9cf2446268e2b63d3cd067c
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-15 23:46:43 +0100

    Remove unused relation in user and post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,12 +7,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    
-    public function user():BelongsTo {
-
-        return $this->belongsTo(User::class);
-    }
-
     public function category():BelongsTo {
 
         return $this->belongsTo(Category::class);
