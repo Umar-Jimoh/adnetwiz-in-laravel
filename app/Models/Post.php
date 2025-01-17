64c05ce138e732429820440d051e463dbd1ee281
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-17 10:29:57 +0100

    Add scope for filtering published posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\PostStatusEnum;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -12,20 +14,26 @@ class Post extends Model implements HasMedia
 {
     use InteractsWithMedia;
 
+    public static function scopePublished(Builder $query): Builder
+    {
+        return $query->where('status', PostStatusEnum::Published);
+    }
+
     public function registerMediaConversions(?Media $media = null): void
     {
         // Thumbnail for recent/popular posts
         $this->addMediaConversion('thumbnail')
-        ->width(90);
+            ->width(90);
         // Image for post lists (e.g., cards)
         $this->addMediaConversion('card')
-        ->width(330);
+            ->width(330);
         // Featured image for the post page
         $this->addMediaConversion('featured')
-        ->width(620);
+            ->width(620);
     }
 
-    public function category():BelongsTo {
+    public function category(): BelongsTo
+    {
 
         return $this->belongsTo(Category::class);
     }
