563d105240a1f591eb2bc048022f767527a85ef9
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-18 01:20:15 +0100

    Fetch and display posts based on category slug

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostListResource;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
     function show(string $slug): View
-    {
-        return view('category.show');
+    {   
+        $category = Category::where('slug', $slug)->firstOrFail();
+
+        $posts = Post::where('category_id', $category->id)->published()->paginate(12);
+
+        return view('category.show', ['posts' => PostListResource::collection($posts)->response()->getData(), 'category' => $category]);
     }
 }
